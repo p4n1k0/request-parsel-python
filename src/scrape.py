b1c1f37ee28d7cc1162fbc0787b9b4381bbbc71d
p4n1k0 <gabre.tarick@gmail.com>
2023-03-10 17:33:35 -0300

    requisicao, extracao d campos com img da capa do livro

--- src/scrape.py
+++ src/scrape.py
@@ -1,21 +1,23 @@
 import requests
 import parsel
 
-URL_BASE = "http://books.toscrape.com/catalogue/"
-response = requests.get(URL_BASE + "the-grand-design_405/index.html")
-selector = parsel.Selector(response.text)
 
-title = selector.css("h1::text").get()
+def scrape(url: str) -> str:
+    URL_BASE = "http://books.toscrape.com/catalogue/"
+    response = requests.get(URL_BASE + "the-grand-design_405/index.html")
+    selector = parsel.Selector(response.text)
 
-price = selector.css(
-    '.product_main > .price_color::text'
-    ).re_first(r'\d*\.\d{2}')
+    title = selector.css("h1::text").get()
 
-description = selector.css('#product_description ~ p::text').get()
-suffix = '...more'
-if description.endswith(suffix):
-    description = description[:-len(suffix)]
+    price = selector.css(".product_main > .price_color::text").re_first(
+            r"\d*\.\d{2}"
+            )
+    description = selector.css("#product_description ~ p::text").get()
+    cover = URL_BASE + selector.css("img::attr(src)").get()
+    suffix = "...more"
+    if description.endswith(suffix):
+        description = description.removesuffix(suffix)
 
-cover = URL_BASE + selector.css('#product_gallery img::attr(src)').get()
-
-print(title, price, description, cover, sep=',')
+    result = [title, price, description, cover]
+    format_result = ",".join(result)
+    return format_result
